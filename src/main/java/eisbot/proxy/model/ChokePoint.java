9c51c726a560cf46191afd46a0fcb5a41b0429ef
Marcus McCurdy <marcus.mccurdy@gmail.com>
2012-03-02 08:41:04 -0500

    deprected getCenterX and getCenterY in choke point added getCenterPosition

--- src/main/java/eisbot/proxy/model/ChokePoint.java
+++ src/main/java/eisbot/proxy/model/ChokePoint.java
@@ -1,91 +1,116 @@
 package eisbot.proxy.model;
+
 /**
  * Represents a choke point in a StarCraft map.
- * 
- * For a description of fields see: http://code.google.com/p/bwta/wiki/Chokepoint
+ *
+ * For a description of fields see:
+ * http://code.google.com/p/bwta/wiki/Chokepoint
  */
 public class ChokePoint {
-	
-	public static final int numAttributes = 9;
-	public static final double fixedScale = 100.0;
-	
-	private int centerX;
-	private int centerY;
-	private double radius;
-	private int firstRegionID;
-	private int secondRegionID;
-	private int firstSideX;
-	private int firstSideY;
-	private int secondSideX;
-	private int secondSideY;
-	private Region firstRegion;
-	private Region secondRegion;
 
-	public ChokePoint(int[] data, int index) {
-		centerX = data[index++];
-		centerY = data[index++];
-		radius = ((double)data[index++])/fixedScale;
-		firstRegionID = data[index++];
-		secondRegionID = data[index++];
-		firstSideX = data[index++];
-		firstSideY = data[index++];
-		secondSideX = data[index++];
-		secondSideY = data[index++];
-	}
-	
-	public Region getOtherRegion(Region region) {
-		return region.equals(firstRegion) ? secondRegion : firstRegion;
-	}
+    public static final int numAttributes = 9;
+    public static final double fixedScale = 100.0;
+    private Position center;
+    private int centerX;
+    private int centerY;
+    private double radius;
+    private int firstRegionID;
+    private int secondRegionID;
+    private int firstSideX;
+    private int firstSideY;
+    private int secondSideX;
+    private int secondSideY;
+    private Region firstRegion;
+    private Region secondRegion;
 
-	public Region getFirstRegion() {
-		return firstRegion;
-	}
+    public ChokePoint(int[] data, int index) {
+        centerX = data[index++];
+        centerY = data[index++];
+        center = new Position(centerX, centerY);
+        radius = ((double) data[index++]) / fixedScale;
+        firstRegionID = data[index++];
+        secondRegionID = data[index++];
+        firstSideX = data[index++];
+        firstSideY = data[index++];
+        secondSideX = data[index++];
+        secondSideY = data[index++];
+    }
 
-	public void setFirstRegion(Region firstRegion) {
-		this.firstRegion = firstRegion;
-	}
+    public Region getOtherRegion(Region region) {
+        return region.equals(firstRegion) ? secondRegion : firstRegion;
+    }
 
-	public Region getSecondRegion() {
-		return secondRegion;
-	}
+    public Region getFirstRegion() {
+        return firstRegion;
+    }
 
-	public void setSecondRegion(Region secondRegion) {
-		this.secondRegion = secondRegion;
-	}
+    public void setFirstRegion(Region firstRegion) {
+        this.firstRegion = firstRegion;
+    }
 
-	public int getCenterX() {
-		return centerX;
-	}
+    public Region getSecondRegion() {
+        return secondRegion;
+    }
 
-	public int getCenterY() {
-		return centerY;
-	}
+    public void setSecondRegion(Region secondRegion) {
+        this.secondRegion = secondRegion;
+    }
 
-	public double getRadius() {
-		return radius;
-	}
+    /**
+     * Use getCenter() instead
+     * @return
+     * @see #getCenter()
+     * @deprecated
+     */
+    @Deprecated
+    public int getCenterX() {
+        return centerX;
+    }
 
-	public int getFirstRegionID() {
-		return firstRegionID;
-	}
+    /**
+     * Returns the Position of this ChokePoints center.
+     * @return the Position that represents this ChokePoints center.
+     */
+    public Position getCenter() {
+        return center;
+    }
 
-	public int getSecondRegionID() {
-		return secondRegionID;
-	}
+    /**
+     * Use getCenter() instead
+     * @see #getCenter() 
+     * @return
+     * @deprecated
+     */
+    @Deprecated
+    public int getCenterY() {
+        return centerY;
+    }
 
-	public int getFirstSideX() {
-		return firstSideX;
-	}
+    public double getRadius() {
+        return radius;
+    }
 
-	public int getFirstSideY() {
-		return firstSideY;
-	}
+    public int getFirstRegionID() {
+        return firstRegionID;
+    }
 
-	public int getSecondSideX() {
-		return secondSideX;
-	}
+    public int getSecondRegionID() {
+        return secondRegionID;
+    }
 
-	public int getSecondSideY() {
-		return secondSideY;
-	}
+    public int getFirstSideX() {
+        return firstSideX;
+    }
+
+    public int getFirstSideY() {
+        return firstSideY;
+    }
+
+    public int getSecondSideX() {
+        return secondSideX;
+    }
+
+    public int getSecondSideY() {
+        return secondSideY;
+    }
 }
